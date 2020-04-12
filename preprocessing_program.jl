bc1dcb271c055988e59102b847c07b9680ecd979
14741Saba <60242166+14741Saba@users.noreply.github.com>
2020-04-12 02:53:37 -0400

    idk man

--- preprocessing_program.jl
+++ preprocessing_program.jl
@@ -2,10 +2,12 @@
 import Missings
 import Base
 
+
 using CSV
 using DataFrames
 using Missings
 using Base
+
 
 gbm_df = CSV.File("$(Base.Filesystem.pwd())/gbm_final_data.csv", header = 1, footerskip = 0) |> DataFrame
 gbm_final_df = gbm_df[:, [1,3,4,5,6,7,8,9,10]]
@@ -26,17 +28,19 @@ end
     gbm_final_df[gbm_final_df[:Sex] .== "Female", :Sex] = 2
 =#
 # dictionary: race (saba)
-
-for row in 1:nrows
+# dictionary didn't do squat :( 
+#=for race_value in gbm_final_df
     race_dict = Dict()
     race_dict["ASIAN"] = "1"
     race_dict["BLACK OR AFRICAN AMERICAN"] = "2"
     race_dict["WHITE"] = "3"
-        if haskey(race_dict, row)
-            print("$(race_dict[row])")
+    race_dict["missing"] = "-1"
+        if haskey(race_dict, race_value)
+            print("$(race_dict[race_value])")
         end
-    end
-
+    end 
+=#
+print(gbm_final_df)
 
 
 
