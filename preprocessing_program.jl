68422b4d22be9d6c3e8403939d4513fd4dd038a5
jlin76 <60355792+jlin76@users.noreply.github.com>
2020-04-11 21:36:25 -0500

    commit but not super helpful rn

--- preprocessing_program.jl
+++ preprocessing_program.jl
@@ -3,7 +3,7 @@
 using CSV
 using DataFrames
 
-gbm_df = CSV.File("/Users/michellemai/gbm_ml/gbm_final_data.csv", header = 1, footerskip = 0) |> DataFrame
+gbm_df = CSV.File("/Users/johnlin/github/gbm_ml/gbm_final_data.csv", header = 1, footerskip = 0) |> DataFrame
 gbm_final_df = gbm_df[:, [1,3,4,5,6,7,8,9,10]]
 
 # for every empty unit, replace with -1 (michelle)
@@ -14,7 +14,14 @@ for row in 1:nrows
     end
 end
 
-# dictionary (2): sex (john), race category (saba)
+# dictionary: sex (john)
+
+#for row in 1:nrows
+    gbm_final_df[:Sex] = convert(DataArray{Union(String, Int64), 1}, gbm_final_df[:Sex])
+    gbm_final_df[gbm_final_df[:Sex] .== "Male", :Sex] = 1
+    gbm_final_df[gbm_final_df[:Sex] .== "Female", :Sex] = 2
+#end
+# dictionary: race (saba)
 
 
 
