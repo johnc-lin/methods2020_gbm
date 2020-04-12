c814e4b5e5a353fb6efd139904d3f13f594a33e1
jlin76 <60355792+jlin76@users.noreply.github.com>
2020-04-11 21:41:47 -0500

    fixed Missings package

--- preprocessing_program.jl
+++ preprocessing_program.jl
@@ -7,24 +7,24 @@ using DataFrames
 using Missings
 using Base
 
-gbm_df = CSV.File("$(Base.Filesystem.pwd())/gbm_ml/gbm_final_data.csv", header = 1, footerskip = 0) |> DataFrame
+gbm_df = CSV.File("$(Base.Filesystem.pwd())/gbm_final_data.csv", header = 1, footerskip = 0) |> DataFrame
 gbm_final_df = gbm_df[:, [1,3,4,5,6,7,8,9,10]]
 
 # for every empty unit, replace with -1 (michelle)
 nrows, ncols = size(gbm_final_df)
 for row in 1:nrows
     for col in 1:ncols
-        missings.replace(x, -1)
+        Missings.replace(col, -1)
     end
 end
 
 # dictionary: sex (john)
 
-#for row in 1:nrows
+#=for row in 1:nrows
     gbm_final_df[:Sex] = convert(DataArray{Union(String, Int64), 1}, gbm_final_df[:Sex])
     gbm_final_df[gbm_final_df[:Sex] .== "Male", :Sex] = 1
     gbm_final_df[gbm_final_df[:Sex] .== "Female", :Sex] = 2
-#end
+=#
 # dictionary: race (saba)
 
 
