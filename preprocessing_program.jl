956a8d1c69cf9f238e0ad82d668e82948781fd2d
14741Saba <60242166+14741Saba@users.noreply.github.com>
2020-04-14 22:07:01 -0400

    hello

--- preprocessing_program.jl
+++ preprocessing_program.jl
@@ -7,6 +7,7 @@ using CSV
 using DataFrames
 using Missings
 using Base
+using Statistics
 
 
 gbm_df = CSV.File("$(Base.Filesystem.pwd())/gbm_final_data.csv", header = 1, footerskip = 0) |> DataFrame
@@ -15,6 +16,7 @@ gbm_final_df = gbm_df[:, [1,3,4,5,6,7,8,9,10]]
 # for every missing unit, replace with -1 (michelle)
 gbm_final_df = coalesce.(gbm_final_df, -1)
 #print(gbm_final_df)
+
 
 
 # John: Assign each patient a numerical value based on their sex:
@@ -41,12 +43,20 @@ for row in gbm_final_df[!, 6]
         gbm_final_df[!, 6][s] = 3
     end
 end
-
+#=
+convert(AbstractFloat, gbm_final_df[!, 3])
+a=0
+for missing_value in gbm_final_df[!, 3]
+    global a += 1
+    if missing_value[a] == -1
+        gbm_final_df[!, 3][a] = mean(gbm_final_df[!, 3])
+    end
+end
 #println(gbm_final_df.Sex)
 #println(gbm_final_df[!, 6])
+ =#
 
-
-CSV.write("/Users/sabaparacha/methods2020/gbm_ml/processed_data.csv", gbm_final_df)
+#CSV.write("/Users/sabaparacha/methods2020/gbm_ml/processed_data.csv", gbm_final_df)
 
 
 
