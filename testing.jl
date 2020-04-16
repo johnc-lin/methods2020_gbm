c2807b600665814f9a1ea0a12a2939a9a4c76bd4
michellemai29 <60238112+michellemai29@users.noreply.github.com>
2020-04-15 21:10:36 -0500

    woo first draft of decision tree regression

--- testing.jl
+++ testing.jl
@@ -2,14 +2,11 @@ using DecisionTree
 using DataFrames
 
 include("preprocessing_program.jl")
-loading_dataset()
-
-# labels_training, features_training = load_data("gbm_testing_array")
-println(labels_training)
+tr_features, tr_labels, test_features, test_leables = get_dataset()
 
 # build model using training data
-# uses 3 random features, 100 trees, 0.7 portions of samples per tree, 5 samples per leaf
-# model = build_forest( 3, 100, 0.7, 5)
+# uses 3 random features, 100 trees, 0.7 portions of samples per tree, 5 samples per leafjuli
+model = build_forest(tr_labels, tr_features, 3, 100, 0.7, 5)
 
 # #apply model 
-# apply_forest(model, )
+print(apply_forest(model, test_features[1,:]))
